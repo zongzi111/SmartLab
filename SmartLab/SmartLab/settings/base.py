7ad802ecf66e3cd938f6973725a7d6c685e6bafb
zongzi <haolin1021@gmail.com>
2023-01-31 15:34:31 +0800

    add simpleui theme plug-in

--- SmartLab/SmartLab/settings/base.py
+++ SmartLab/SmartLab/settings/base.py
@@ -28,13 +28,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'instrument.apps.InstrumentConfig',
+    #'instrument.apps.InstrumentConfig',
 ]
 
 MIDDLEWARE = [
